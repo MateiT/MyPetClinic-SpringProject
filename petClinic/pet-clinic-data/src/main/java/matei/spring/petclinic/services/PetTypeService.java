e6b4f173a1844d0bbb862bdcca4d83a9952d2e7b
MateiT <tanasemat@gmail.com>
2022-02-18 11:59:28 +0200

    Implement map based services

---
+++ petClinic/pet-clinic-data/src/main/java/matei/spring/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package matei.spring.petclinic.services;
+
+import matei.spring.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ petClinic/pet-clinic-data/src/main/java/matei/spring/petclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,38 @@
+package matei.spring.petclinic.services.map;
+
+import matei.spring.petclinic.model.Owner;
+import matei.spring.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    Owner save(Long aLong, Owner object) {
+        return super.save(aLong, object);
+    }
+}
