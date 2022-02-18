7e5faafe66fc7a1e4bc5092cb21b0d71941a7c74
MateiT <tanasemat@gmail.com>
2022-02-18 11:39:02 +0200

    Refactor service interfaces

--- petClinic/pet-clinic-data/src/main/java/matei/spring/petclinic/services/PetService.java
+++ petClinic/pet-clinic-data/src/main/java/matei/spring/petclinic/services/PetService.java
@@ -2,10 +2,5 @@ package matei.spring.petclinic.services;
 
 import matei.spring.petclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long>{
 }
