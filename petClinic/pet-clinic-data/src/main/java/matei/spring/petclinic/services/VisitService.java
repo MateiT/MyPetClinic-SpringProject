031bc6aaf7d9ed6e693e3755c092ffa1b7ba2eb1
MateiT <tanasemat@gmail.com>
2022-02-22 19:21:44 +0200

    Add jpa & map service implementation for visit

---
+++ petClinic/pet-clinic-data/src/main/java/matei/spring/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package matei.spring.petclinic.services;
+
+import matei.spring.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+}
---
+++ petClinic/pet-clinic-data/src/main/java/matei/spring/petclinic/services/jpa/VisitJpaService.java
@@ -0,0 +1,48 @@
+package matei.spring.petclinic.services.jpa;
+
+import matei.spring.petclinic.model.Visit;
+import matei.spring.petclinic.repositories.VisitRepository;
+import matei.spring.petclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class VisitJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
