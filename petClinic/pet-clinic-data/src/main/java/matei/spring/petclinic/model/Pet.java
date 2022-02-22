687b77b407b46776421b2b2268e7a879229a09a1
MateiT <tanasemat@gmail.com>
2022-02-22 18:33:01 +0200

    Convert entities to use JPA

--- petClinic/pet-clinic-data/src/main/java/matei/spring/petclinic/model/Pet.java
+++ petClinic/pet-clinic-data/src/main/java/matei/spring/petclinic/model/Pet.java
@@ -1,12 +1,27 @@
 package matei.spring.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity{
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
